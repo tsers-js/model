4c04b4663b40027f245fcf2d38891aaedccc2c62
Matti Lankinen <m.lankinen@iki.fi>
2016-03-26 02:48:11 +0200

    Fix original stanga implementation to TSERS

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
+import Rx, {Observable as O} from "rx"
 import R_ from "ramda"
 import L_ from "partial.lenses"
+
+const extend = Object.assign
 
 export const R = R_
 
@@ -13,9 +16,9 @@ export default function makeModel(initial, opts = {}) {
     warn = (...args) => console.warn(...args)       // eslint-disable-line
     } = opts
 
-
-  return function Model(mod$) {
-    let state$ = mod$
+  return function Model() {
+    let obs = null, dispose = null
+    let state$ = O.create(o => (obs = o) && (() => obs = null))
       .filter(m => (m && m.ID === ID) || (warn(
         "Received modification that was not created by using 'M.mod' or 'M.set'. Ignoring...", m
       ) && false))
@@ -25,18 +28,28 @@ export default function makeModel(initial, opts = {}) {
     if (logging) {
       state$ = state$.do(s => info("New state:", s))
     }
-    return model(state$, R.lens(R.identity, R.nthArg(0)))
 
-    function model(state$, stateLens) {
-      const val$ = state$
-        .distinctUntilChanged()
-        .shareReplay(1)
+    state$ = state$.replay(null, 1)
+    dispose = state$.connect()
 
-      return Object.assign(val$, {
-        lens: (l, ...ls) => model(val$.map(L.view(L(l, ...ls))), L(stateLens, l, ...ls)),
-        mod: mod$ => mod$.map(mod => ({mod: R.over(stateLens, mod), ID})),
-        set: val$ => val$.map(val => ({mod: R.over(stateLens, R.always(val)), ID}))
-      })
-    }
+    const M = model(state$, R.lens(R.identity, R.nthArg(0)))
+    const executor = output$ => new Rx.CompositeDisposable(
+      dispose,
+      output$.subscribe(mod => obs && obs.onNext(mod))
+    )
+
+    return [M, executor]
+  }
+
+  function model(state$, stateLens) {
+    const val$ = state$
+      .distinctUntilChanged()
+      .shareReplay(1)
+
+    return extend(val$, {
+      lens: (l, ...ls) => model(val$.map(L.view(L(l, ...ls))), L(stateLens, l, ...ls)),
+      mod: mod$ => mod$.map(mod => ({mod: R.over(stateLens, mod), ID})),
+      set: val$ => val$.map(val => ({mod: R.over(stateLens, R.always(val)), ID}))
+    })
   }
 }
